07c5ccdedeb8f206fcb9fc15026d4533564a3443
matsumura-taichi <hiroto.in.the.cromagnons@gmail.com>
2017-03-08 00:12:40 +0900

    Initial Commit

---
+++ SimpleBrowser/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SimpleBrowser
+//
+//  Created by 松村泰地 on 2017/03/08.
+//  Copyright © 2017年 松村泰地. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
